f95a95fe2d9fdc9d3b3a4af05ab0c8c486ac4fcd
くぁーりぃ <kaito6343@gmail.com>
2023-02-11 21:13:11 +0900

    display illegal word on command

--- src/main/java/quarri6343/katakanaspectator/OutCommand.java
+++ src/main/java/quarri6343/katakanaspectator/OutCommand.java
@@ -2,6 +2,7 @@ package quarri6343.katakanaspectator;
 
 import net.kunmc.lab.commandlib.Command;
 import net.kunmc.lab.commandlib.argument.PlayerArgument;
+import net.kunmc.lab.commandlib.argument.StringArgument;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
@@ -10,11 +11,11 @@ import org.bukkit.GameMode;
 public class OutCommand extends Command {
     public OutCommand() {
         super("out");
-
-        argument(new PlayerArgument("player"), (player, ctx) -> {
+        
+        argument(new PlayerArgument("player"),new StringArgument("word"), (player, string, ctx) -> {
             player.getLocation().createExplosion(4, false, false);
             player.setGameMode(GameMode.SPECTATOR);
-            Bukkit.broadcast(Component.text(player.getName() + "が外来語を使用しました！").color(NamedTextColor.RED));
+            Bukkit.broadcast(Component.text(player.getName() + "が外来語「" + string  + "」を使用しました！").color(NamedTextColor.RED));
         });
     }
 }
