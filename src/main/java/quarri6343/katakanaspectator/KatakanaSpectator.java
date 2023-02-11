b3c5b2457faf06be9d75a6d2f2710c53e7fcc61a
くぁーりぃ <kaito6343@gmail.com>
2023-02-11 19:12:12 +0900

    improve judge method

--- src/main/java/quarri6343/katakanaspectator/KatakanaSpectator.java
+++ src/main/java/quarri6343/katakanaspectator/KatakanaSpectator.java
@@ -4,6 +4,7 @@ import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.serializer.plain.PlainComponentSerializer;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
@@ -27,7 +28,7 @@ public final class KatakanaSpectator extends JavaPlugin implements Listener {
     public void onPlayerChat(AsyncChatEvent e){
         String message = new PlainComponentSerializer().serialize(e.message());
 
-        if(isAlphabet(message) || isKana(message) || isKanaHalf(message) || isNumber(message)){
+        if(!isZenkaku(message) || isKana(message) || isKanaHalf(message)){
             if(e.getPlayer().getGameMode() == GameMode.SURVIVAL){
                 Component component = Component.text(message).color(NamedTextColor.RED);
                 e.message(component);
@@ -52,22 +53,16 @@ public final class KatakanaSpectator extends JavaPlugin implements Listener {
             }
         }
     }
-
-    /**
-     * 英字チェック
-     * https://medium-company.com/java-%e8%8b%b1%e5%ad%97-%e3%83%81%e3%82%a7%e3%83%83%e3%82%af/
-     * @param value 検証対象の値
-     * @return 結果（true：英字、false：英字ではない）
-     */
-    public static boolean isAlphabet(String value) {
-        boolean result = false;
-        if (value != null) {
-            Pattern pattern = Pattern.compile("^.*[a-zA-Z].*$");
-            result = pattern.matcher(value).matches();
-        }
-        return result;
-    }
     
+    public static boolean isZenkaku(String text) {
+        byte[] bytes = text.getBytes();
+        if (text.length() != bytes.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     /**
      * 全角カタカナチェック
      * https://medium-company.com/java-%E5%85%A8%E8%A7%92%E3%82%AB%E3%83%8A-%E3%83%81%E3%82%A7%E3%83%83%E3%82%AF/
@@ -82,7 +77,7 @@ public final class KatakanaSpectator extends JavaPlugin implements Listener {
         }
         return result;
     }
-    
+
     /**
      * 半角カタカナチェック
      * https://medium-company.com/java-%E5%8D%8A%E8%A7%92%E3%82%AB%E3%83%8A-%E3%83%81%E3%82%A7%E3%83%83%E3%82%AF/
@@ -93,15 +88,6 @@ public final class KatakanaSpectator extends JavaPlugin implements Listener {
         boolean result = false;
         if (value != null) {
             Pattern pattern = Pattern.compile("^.*[\uFF65-\uFF9F].*$");
-            result = pattern.matcher(value).matches();
-        }
-        return result;
-    }
-    
-    public static boolean isNumber(String value){
-        boolean result = false;
-        if (value != null) {
-            Pattern pattern = Pattern.compile("^.*[0-9].*$");
             result = pattern.matcher(value).matches();
         }
         return result;
